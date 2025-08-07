73f9605fcc5d32a7ddccd4e9cea52ec7bf1fd8b2
smw-1211 <wattamwarsoham2000@gmail.com>
2025-08-07 20:17:44 +0530

    Adding a new component - NewPost

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,44 @@
+import classes from './NewPost.module.css';
+import {useState} from "react";
+
+function NewPost({onAddPost, onCancel}) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler (e) {
+      setEnteredBody(e.target.value);
+  }
+
+  function changeAuthorHandler (e) {
+      setEnteredAuthor(e.target.value);
+  }
+
+  function submitHandler (e) {
+    e?.preventDefault();
+    const postData = {
+      body: enteredBody,
+      author: enteredAuthor
+    }
+    onAddPost(postData);
+    onCancel();
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required onChange={changeAuthorHandler} />
+      </p>
+      <p className={classes.actions}>
+        <button type='button' onClick={onCancel}>Cancel</button>
+        <button>Submit</button>
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
